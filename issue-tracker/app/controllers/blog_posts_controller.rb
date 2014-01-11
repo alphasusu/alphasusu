fa143a106d3efc20f2c6aba2dc7564c0b3192324
Elliot Hughes <elliot.hughes@gmail.com>
2014-01-11 18:44:23 +0000

    Blog posts on home page are real things

--- issue-tracker/app/controllers/blog_posts_controller.rb
+++ issue-tracker/app/controllers/blog_posts_controller.rb
@@ -2,7 +2,28 @@ class BlogPostsController < ApplicationController
   def show
     @post = BlogPost.find(params[:id])
   end
+  
   def index
     @posts = BlogPost.all
+  end
+  
+  def create
+    @post = BlogPost.new(post_params)
+    
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def new 
+    @post = BlogPost.new
+  end
+
+private
+
+  def post_params
+    params.require(:blog_post).permit(:title, :body)
   end
 end
