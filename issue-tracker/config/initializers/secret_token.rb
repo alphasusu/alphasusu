98cf4690bf534e242ea215ea327510da205ca1b6
Elliot Hughes <elliot.hughes@gmail.com>
2014-01-11 16:59:09 +0000

    Setup basic rails site

---
+++ issue-tracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IssueTracker::Application.config.secret_key_base = 'f2fcc99384b1764f7315b092811c6e2c3abe43ae7f49d310c181e39b0505318ff56422831302f7eafbac53964268ba01e155b5308fed8cd0bc061d783fd7539c'
