c7f7a6500426efa6acb8f63e64acbcee7a460cd2
Elliot Hughes <elliot.hughes@gmail.com>
2014-01-11 17:37:19 +0000

    WIP New

--- issue-tracker/app/controllers/issues_controller.rb
+++ issue-tracker/app/controllers/issues_controller.rb
@@ -6,4 +6,25 @@ class IssuesController < ApplicationController
   def show
     @issue = Issue.find(params[:id])
   end
+
+  def create
+    @issue = Issue.new(params[:post])
+    
+    if @issue.save
+      redirect_to @issue
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @issue = Issue.new
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+
 end
