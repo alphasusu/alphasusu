86da1956e2262392f8c8bef606d54982629b6498
Dan Palmer <dan.palmer@me.com>
2014-01-11 17:59:19 +0000

    Installed Devise and LDAP auth

--- issue-tracker/config/routes.rb
+++ issue-tracker/config/routes.rb
@@ -1,4 +1,5 @@
 IssueTracker::Application.routes.draw do
+  devise_for :users
   resources :issues
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
