550a14814fedc030e32987223850b05645dc9f38
Elliot Hughes <elliot.hughes@gmail.com>
2014-01-11 17:22:09 +0000

    Add issue show, index and models

--- issue-tracker/db/schema.rb
+++ issue-tracker/db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111170357) do
+ActiveRecord::Schema.define(version: 20140111172012) do
 
   create_table "ideas", force: true do |t|
     t.string "type"
     t.string "title"
     t.string "description"
+  end
+
+  create_table "issues", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
