a0c68e12774bcf9aa6e37fdeb587dfb222bc1a14
Dan Palmer <dan.palmer@me.com>
2014-01-12 01:11:43 +0000

    First run check on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :check_first_run
+
+  def check_first_run
+    if not cookies.permanent[:returning_user]
+      @first_visit = true
+      cookies.permanent[:returning_user] = true
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,6 @@ class HomeController < ApplicationController
 	def index
         @posts = BlogPost.all
         @events = Event.all
-
-        if not cookies.permanent[:returning_user]
-        	@first_visit = true
-        	cookies.permanent[:returning_user] = true
-        end
 	end
 	
 end
