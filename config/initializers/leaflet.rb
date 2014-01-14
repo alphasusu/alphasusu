03e932c7130002775468f165edfcaa9344d02c96
Dan Palmer <dan.palmer@me.com>
2014-01-14 00:03:28 +0000

    Added Leaflet, waiting for Foundation 5.0.3 for bug fixes

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,3 @@
+Leaflet.tile_layer = "http://tile.stamen.com/toner/{z}/{x}/{y}.png"
+Leaflet.attribution = "Map tiles by <a href=\"http://stamen.com\">Stamen Design</a>, under <a href=\"http://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a>. Data by <a href=\"http://openstreetmap.org\">OpenStreetMap</a>, under <a href=\"http://creativecommons.org/licenses/by-sa/3.0\">CC BY SA</a>."
+Leaflet.max_zoom = 18
