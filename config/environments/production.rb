44a788ed832e04b6bef8724856279755583821c7
Dan Palmer <dan.palmer@me.com>
2014-01-15 20:18:47 +0000

    Fix, should be a regex rather than an array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,7 @@ IssueTracker::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # Compile *everything* that doesn't begin with an underscore.
-  config.assets.precompile += /(^[^_\/]|\/[^_])[^\/]*$/
+  config.assets.precompile = /(^[^_\/]|\/[^_])[^\/]*$/
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
