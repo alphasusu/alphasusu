4c8b471dee6c83889b4f57e141fbe1e58d2d23eb
Elliot Hughes <elliot.hughes@gmail.com>
2014-01-15 02:06:47 +0000

    Show Union President

--- app/controllers/democracy_controller.rb
+++ app/controllers/democracy_controller.rb
@@ -3,6 +3,7 @@ class DemocracyController < ApplicationController
   skip_authorization_check
 
   def index
+    @president = Officer.where(:title => "Union President")
     @vice_presidents = Officer.where.not(:title => "Union President")
   end
 end
