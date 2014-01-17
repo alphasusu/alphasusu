9c3a3ab7e4623174bfff985e92a262e4ad0a5efe
Dan Palmer <dan.palmer@me.com>
2014-01-17 15:17:50 +0000

    After create, query LDAP for first/last name

--- app/models/ldap_user.rb
+++ app/models/ldap_user.rb
@@ -1,3 +1,11 @@
 class LdapUser < User
   devise :ldap_authenticatable, :rememberable, :trackable
+  
+  after_create :get_ldap_names
+  
+  def get_ldap_names
+    self.first_name = Devise::LDAP::Adapter.get_ldap_param(self.email, 'givenName').first
+    self.last_name = Devise::LDAP::Adapter.get_ldap_param(self.email, 'sn').first
+    self.save
+  end
 end
