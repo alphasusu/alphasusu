c356dd2aa0e21938d1e2ea0ac78f6acb2c4c6a4a
Dan Palmer <dan.palmer@me.com>
2014-01-17 23:49:48 +0000

    Added avatar to user, with gravatar fallback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,32 @@
 class User < ActiveRecord::Base
+	
+	has_attached_file(
+		:avatar,
+		:styles => { :small => "40x40>", :medium => "100x100>", :large => "300x300" }
+	)
+	
+	validates_attachment :avatar, :size => { :in => 0..200.kilobytes }
+	
 	def name
 		"#{self.first_name} #{self.last_name}"
+	end
+	
+	def get_avatar_url(size)
+		if self.avatar.present?
+			self.avatar.url(size)
+		else
+			"http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=#{dimension_for_size(size)}&d=mm"
+		end
+	end
+	
+	def dimension_for_size(size)
+		case size
+			when :small
+				40
+			when :medium
+				100
+			when :large
+				300
+		end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117175637) do
+ActiveRecord::Schema.define(version: 20140117211103) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -226,6 +226,10 @@ ActiveRecord::Schema.define(version: 20140117175637) do
     t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
