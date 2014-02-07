4c808124418379f50f959defbbc90927b608a244
Elliot Hughes <elliot.hughes@gmail.com>
2014-02-07 20:46:18 +0000

    Correct regexp for february

--- db/seeds.rb
+++ db/seeds.rb
@@ -188,7 +188,7 @@ open(whats_on) do |rss|
 
   feed.items.each do |item|
     event = Event.new
-    title_split = item.title.split(/((?:Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday), [0-9]{1,2}(?:st|nd|th|rd) (?:January|Feburary|March|April|May|June|July|August|September|October|November|December) [0-9]{4} - [0-9]{1,2}:[0-9]{2}(?:a|p)m)(?: )(.+ )(?:(?:in|at) )(.*)/)
+    title_split = item.title.split(/((?:Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday), [0-9]{1,2}(?:st|nd|th|rd) (?:January|February|March|April|May|June|July|August|September|October|November|December) [0-9]{4} - [0-9]{1,2}:[0-9]{2}(?:a|p)m)(?: )(.+ )(?:(?:in|at) )(.*)/)
     if title_split[1] and title_split[2]
       event.title = title_split[2]
       event.date = DateTime.parse(title_split[1])
