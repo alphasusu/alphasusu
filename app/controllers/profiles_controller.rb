4cf7507b4b50d4ea22184f0043aa6f2a7f681e1c
Dan Palmer <dan.palmer@me.com>
2014-02-08 10:00:20 +0000

    Allow setting of course on user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,27 @@
 class ProfilesController < ApplicationController
-	before_filter :require_login, except: [ :show ]
-	rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
-  
-	def show
-	  	@profile = User.find(params[:id])
-	end
-  	
-	def me
-		@profile = current_user
-		render :show
-	end
-  
-  	def edit
-	  	@profile = current_user
-	end
-	
-	def update
+    before_filter :require_login, except: [ :show ]
+    rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+    
+    def show
+        @profile = User.find(params[:id])
+    end
+        
+    def me
+        @profile = current_user
+        render :show
+    end
+    
+    def edit
+        @profile = current_user
+    end
+    
+    def update
         updates = profile_params
-		@profile = User.find(params[:id])
-		
-		@profile.first_name = updates[:first_name]
-		@profile.last_name = updates[:last_name]
+        @profile = User.find(params[:id])
+        
+        @profile.first_name = updates[:first_name]
+        @profile.last_name = updates[:last_name]
+        @profile.course_id = updates[:course_id]
 
         if updates[:avatar]
             @profile.avatar = updates[:avatar]
@@ -29,7 +30,7 @@ class ProfilesController < ApplicationController
         if @profile.is_a?(LocalUser) && updates[:email] != @profile.email
             @profile.email = updates[:email]
         end
-		
+
         success = @profile.save!
 
         if @profile.is_a?(LocalUser) && !updates[:password].blank?
@@ -50,24 +51,24 @@ class ProfilesController < ApplicationController
             flash.now[:alert] = 'Could not save your profile.'
             render action: 'edit'
         end
-	end
-	
-	def destroy
-		
-	end
-  
+    end
+    
+    def destroy
+        
+    end
+    
 private
-  
-  def profile_params
-  	params.require(:profile).permit(
-  		:first_name, :last_name, :email, :avatar,
-  		:password, :password_confirmation, :current_password)
-  end
-  	
-  def user_not_found
-		render text: "User Not Found", status: 404
-    # raise ActionController::RoutingError.new('Not Found')
-    # TODO: make a 'user_not_found' route/view etc, use this for a good 404.
-  end
-	
+    
+    def profile_params
+        params.require(:profile).permit(
+            :first_name, :last_name, :email, :avatar, :course_id,
+            :password, :password_confirmation, :current_password)
+    end
+        
+    def user_not_found
+        render text: "User Not Found", status: 404
+        # raise ActionController::RoutingError.new('Not Found')
+        # TODO: make a 'user_not_found' route/view etc, use this for a good 404.
+    end
+    
 end
