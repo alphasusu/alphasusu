b6414f76d14d695f9e3c24cec3d4e6358400a101
Dan Palmer <dan.palmer@me.com>
2014-02-10 14:34:55 +0000

    Blog Post controller in standard format

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,31 +1,76 @@
 class BlogPostsController < ApplicationController
+  authorize_resource
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
   before_action :set_site_area
   
-  def show
-    @post = BlogPost.friendly.find(params[:id])
-  end
-  
+  # GET /blog_posts
+  # GET /blog_posts.json
   def index
     @posts = BlogPost.all
   end
-  
+
+  # GET /blog_posts/1
+  # GET /blog_posts/1.json
+  def show
+  end
+
+  # GET /blog_posts/new
+  def new
+    @post = BlogPost.new
+  end
+
+  # GET /blog_posts/1/edit
+  def edit
+  end
+
+  # POST /blog_posts
+  # POST /blog_posts.json
   def create
-    @post = BlogPost.new(post_params)
-    
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
+    @post = BlogPost.new(blog_post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Blog Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @post }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  def new 
-    @post = BlogPost.new
+  # PATCH/PUT /blog_posts/1
+  # PATCH/PUT /blog_posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(blog_post_params)
+        format.html { redirect_to @post, notice: 'Blog Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blog_posts/1
+  # DELETE /blog_posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to blog_posts_url }
+      format.json { head :no_content }
+    end
   end
 
 private
 
-  def post_params
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog_post
+    @post = BlogPost.friendly.find(params[:id])
+  end
+
+  def blog_post_params
     params.require(:blog_post).permit(:title, :body)
   end
 end
