e530e78a792dad2f0b5a0f69e644d830dce19a32
Dan Palmer <dan.palmer@me.com>
2014-02-10 15:48:00 +0000

    Permissions descriptions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,2 +1,13 @@
 class Permission < ActiveRecord::Base
+    belongs_to :user
+
+    def description
+        if self.subject_id
+            subject = self.subject_class.constantize.find(self.subject_id)
+            "#{self.user.name} can #{self.action} #{subject.name}"
+        else
+            "#{self.user.name} can #{self.action} #{self.subject_class.pluralize}"
+        end
+    end
+
 end
