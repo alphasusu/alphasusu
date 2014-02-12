cbebd003a78d606becc90b7303cc5f6e5d212072
Dan Palmer <dan.palmer@me.com>
2014-02-12 11:48:33 +0000

    Tests for Menus controller, plus authorise resource

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,5 @@
 class MenusController < ApplicationController
+  authorize_resource
   before_action :set_menu, only: [:show, :edit, :update, :destroy]
   before_action :set_site_area
 
--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MenusControllerTest < ActionController::TestCase
   setup do
-    @menu = menus(:one)
+    @menu = FactoryGirl.create(:menu)
   end
 
   test "should get index" do
@@ -11,12 +11,42 @@ class MenusControllerTest < ActionController::TestCase
     assert_not_nil assigns(:menus)
   end
 
+  test "should not get new without auth" do
+    as_nobody
+    assert_raise(CanCan::AccessDenied) {
+      get :new
+    }
+  end
+
+  test "should not get new without permission" do
+    as_user
+    assert_raise(CanCan::AccessDenied) {
+      get :new
+    }
+  end
+
   test "should get new" do
+    as_admin
     get :new
     assert_response :success
   end
 
+  test "should not create menu without auth" do
+    as_nobody
+    assert_raise(CanCan::AccessDenied) {
+      post :create, menu: {  }
+    }
+  end
+
+  test "should not create menu without permission" do
+    as_user
+    assert_raise(CanCan::AccessDenied) {
+      post :create, menu: {  }
+    }
+  end
+
   test "should create menu" do
+    as_admin
     assert_difference('Menu.count') do
       post :create, menu: {  }
     end
@@ -29,17 +59,62 @@ class MenusControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not get edit without auth" do
+    as_nobody
+    assert_raise(CanCan::AccessDenied) {
+      get :edit, id: @menu
+    }
+  end
+
+  test "should not get edit without permission" do
+    as_user
+    assert_raise(CanCan::AccessDenied) {
+      get :edit, id: @menu
+    }
+  end
+
   test "should get edit" do
+    as_admin
     get :edit, id: @menu
     assert_response :success
   end
 
+  test "should not update menu without auth" do
+    as_nobody
+    assert_raise(CanCan::AccessDenied) {
+      patch :update, id: @menu, menu: {  }
+    }
+  end
+
+  test "should not update menu without permission" do
+    as_user
+    assert_raise(CanCan::AccessDenied) {
+      patch :update, id: @menu, menu: {  }
+    }
+  end
+
   test "should update menu" do
+    as_admin
     patch :update, id: @menu, menu: {  }
     assert_redirected_to menu_path(assigns(:menu))
   end
 
+  test "should not destroy menu without auth" do
+    as_nobody
+    assert_raise(CanCan::AccessDenied) {
+      delete :destroy, id: @menu
+    }
+  end
+
+  test "should not destroy menu without permission" do
+    as_user
+    assert_raise(CanCan::AccessDenied) {
+      delete :destroy, id: @menu
+    }
+  end
+
   test "should destroy menu" do
+    as_admin
     assert_difference('Menu.count', -1) do
       delete :destroy, id: @menu
     end
