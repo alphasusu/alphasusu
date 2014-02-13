a549278adba498578f97e87b11eaf7453cbbd113
Dan Palmer <dan.palmer@me.com>
2014-02-13 17:13:14 +0000

    Set site area on activities for styling

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,16 @@
 class ActivitiesController < ApplicationController
+  before_action :set_site_area
+
   def index
     @society_count = Society.count
     @sport_count = Sport.count
     @performing_art_count = PerformingArt.count
     @media_group_count = MediaGroup.count
+  end
+
+private
+
+  def set_site_area
+    params[:site_area] = :activities
   end
 end
