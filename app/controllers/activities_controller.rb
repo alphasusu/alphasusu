ca5f5fe115fd86871e93b0798e1edff43c39d0ad
Dan Palmer <dan.palmer@me.com>
2014-02-14 16:34:51 +0000

    Querying of student groups updated

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,15 +2,10 @@ class ActivitiesController < ApplicationController
   before_action :set_site_area
 
   def index
-    @sport_count = StudentGroupKind.where(name: "Sport").first.groups.count
-    @performing_art_count = StudentGroupKind.where(name: "Performing Art").first.groups.count
-    @media_group_count = StudentGroupKind.where(name: "Media").first.groups.count
-    @society_count = StudentGroup.count - @sport_count - @performing_art_count - @media_group_count
-
-    groups = StudentGroup.where.not(logo_file_name: nil)
-    groups.shuffle!
-
-    @showcased_groups = groups[0..8]
+    @sport_count = StudentGroupKind.groups_of_type(:sport).count
+    @performing_art_count = StudentGroupKind.groups_of_type(:performing_art).count
+    @media_group_count = StudentGroupKind.groups_of_type(:media).count
+    @society_count = StudentGroupKind.groups_of_type(:society).count
   end
 
 private
