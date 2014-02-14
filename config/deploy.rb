a83aac85c95daff32e7e2040a0691ba60ac17984
Dan Palmer <dan.palmer@me.com>
2014-02-14 23:36:06 +0000

    Fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,8 +34,6 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :git_enable_submodules, 1
-
 namespace :deploy do
 
   desc 'Restart application'
